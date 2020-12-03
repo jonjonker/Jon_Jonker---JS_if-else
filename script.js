2b36e182e1c36b614e56db792a6de67865e32de5
Jón Jonker <66784287+jonjonker@users.noreply.github.com>
2020-12-03 16:48:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const myAge = 19;
+
+if (myAge > 17){
+
+    console.log("please come in, have a wonderful time!")
+}
+
+if (myAge < 18){
+
+    console.log("Please get out, you are too young")
+
+}
+
+const isFemale =false
+
+if (isFemale){
+    console.log("Yes! het is weer ladies night")
+}
+
+if ("false"){
+    console.log("Next time better, buddy! it's ladies night tonight")
+}
+
+const driverStatus ="bob"
+
+if (true){
+
+    console.log("great, you can drive your friends home")
+} 
+
+else{
+
+    console.log("whoops, you are too drunk to drive")
+
+}
+
+
